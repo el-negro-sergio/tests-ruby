fc988d15d66004e97b474e04971c110d6b35b1ab
Rodrigo Vallejos <48982671+el-negro-sergio@users.noreply.github.com>
2019-04-08 22:35:42 +0200

    Delete 06_pig_latin.rb

--- lib/06_pig_latin.rb
+++
@@ -1,8 +0,0 @@
-def translate(a)
-    a.split(" ").map{|x|
-        #(x[0..1]=="qu") ? i = 2 :
-        i=x.index(/[aeiouy]/)
-        (x[i]=="u" && x[i-1]=="q" && x[0...i].match(/[^aeiouy]/)) ? i+=1 : i=i
-        (x[0] =~ /[aeiouy]/) ? (x + "ay") :  x[i..(x.length-1)] + x[0...i] + "ay"
-    }.join(" ")
-end
